511c8c7ee19adc0d38a7dff3dc8ac5026956a090
Deniz_Desktop_Win <deniz-selcuk@outlook.com>
2022-01-17 16:24:57 +0300

    add corelayer

---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Product :BaseEntity
+    {
+        public string? Name { get; set; }
+        public int stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public ProductFeature? ProductFeature { get; set; }
+
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IQueryable<T>> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T,bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
