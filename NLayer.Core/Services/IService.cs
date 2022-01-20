42aabd74f9eba109193355c193e876cd3d75e7bd
Deniz_Desktop_Win <deniz-selcuk@outlook.com>
2022-01-20 14:57:23 +0300

    add DTO models

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -9,11 +9,11 @@ namespace NLayer.Core.Services
     public interface IService<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        Task<IQueryable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T,bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T,bool>> expression);
-        Task AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
